b729382c77dd324931edf407ce67b0beffbcf406
Andrew Liu <aliu1@umd.edu>
2015-05-02 18:20:14 -0400

    removed h flag from symbolic links due to compatibility issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,12 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git pull origin master
 
 # set symbolic links
-ln -sfhv "$DOTFILES_DIR/.aliases" ~
-ln -sfhv "$DOTFILES_DIR/.bash_profile" ~
-ln -sfhv "$DOTFILES_DIR/.bashrc" ~
-ln -sfhv "$DOTFILES_DIR/.exports" ~
-ln -sfhv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfhv "$DOTFILES_DIR/.inputrc" ~
-ln -sfhv "$DOTFILES_DIR/.prompt" ~
-ln -sfhv "$DOTFILES_DIR/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/.aliases" ~
+ln -sfv "$DOTFILES_DIR/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/.exports" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/.prompt" ~
+ln -sfv "$DOTFILES_DIR/.vimrc" ~
 
