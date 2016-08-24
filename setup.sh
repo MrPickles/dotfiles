3271ffe49535191af54bfee7ef03125d2b548022
Andrew Liu <aliu1@umd.edu>
2016-08-23 21:31:42 -0400

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Initialization script to symlink the dotfiles.
+# Based off https://github.com/nicksp/dotfiles/blob/master/setup.sh.
+
+print_success() {
+  # Print output in green
+  printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+print_error() {
+  # Print output in red
+  printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+execute() {
+  $1 &> /dev/null
+  print_result $? "${2:-$1}"
+}
+
+print_result() {
+  [ $1 -eq 0 ] \
+    && print_success "$2" \
+    || print_error "$2"
+
+  [ "$3" == "true" ] && [ $1 -ne 0 ] \
+    && exit
+}
+
+ask_for_confirmation() {
+  print_question "$1 [y/N] "
+  read -n 1
+  printf "\n"
+}
+
+answer_is_yes() {
+  [[ "$REPLY" =~ ^[Yy]$ ]] \
+    && return 0 \
+    || return 1
+}
+
+install_zsh () {
+  # Test to see if zshell is installed.  If it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Install Oh My Zsh if it isn't already present
+    if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      chsh -s $(which zsh)
+    fi
+  else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+      if [[ -f /etc/redhat-release ]]; then
+        sudo yum install zsh
+        install_zsh
+      fi
+      if [[ -f /etc/debian_version ]]; then
+        sudo apt-get install zsh
+        install_zsh
+      fi
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+      echo "We'll install zsh, then re-run this script!"
+      brew install zsh
+      exit
+    fi
+  fi
+}
+
+declare -a FILES_TO_SYMLINK=(
+  'editor/vim'
+  'editor/vimrc'
+
+  'git/gitconfig'
+
+  'shell/dircolors.256dark'
+  'shell/tmux.conf'
+  'shell/zshrc'
+
+  'bin'
+  'powerline'
+)
+
+ask_for_confirmation "Switch to zsh and oh-my-zsh?"
+if answer_is_yes; then
+  install_zsh
+fi
+
+for i in ${FILES_TO_SYMLINK[@]}; do
+  sourceFile="$(pwd)/$i"
+  targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+  if [ ! -e "$targetFile" ]; then
+    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+  elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+    print_success "$targetFile → $sourceFile"
+  else
+    ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+    if answer_is_yes; then
+      rm -rf "$targetFile"
+      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    else
+      print_error "$targetFile → $sourceFile"
+    fi
+  fi
+done
+
