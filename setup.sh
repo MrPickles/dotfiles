49eca849ef87e03ac8e9b49ced3d21d72eee1d4c
Andrew Liu <aliu1@umd.edu>
2017-08-10 23:22:55 -0700

    Rearrange order of operations in setup
    Installing oh-my-zsh would halt the script due to the new shell.
    Instead, we make sure to do that step last, after symlinking, installing
    zsh, and switching to zsh.

--- setup.sh
+++ setup.sh
@@ -45,36 +45,29 @@ answer_is_yes() {
 }
 
 install_zsh () {
-  # Test to see if zshell is installed.  If it is:
-  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Install Oh My Zsh if it isn't already present
-    if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
-      echo "Switching to oh-my-zsh. Please re-run this script afterwards!"
-      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-      chsh -s $(which zsh)
-    fi
-  else
-    # If zsh isn't installed, get the platform of the current machine
+  # Test to see if zshell is installed.
+  if [ -z $(which zsh) ]; then
+    # If zsh isn't installed, get the platform of the current machine and
+    # install zsh with the appropriate package manager.
     platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
       if [[ -f /etc/redhat-release ]]; then
         sudo yum install zsh
-        install_zsh
       fi
       if [[ -f /etc/debian_version ]]; then
         sudo apt-get install zsh
-        install_zsh
       fi
-    # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
-      echo "We'll install zsh, then re-run this script!"
       brew install zsh
-      exit
     fi
+  fi
+  # Set the default shell to zsh if it isn't currently set to zsh
+  if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+    chsh -s $(which zsh)
+  fi
+  # Install Oh My Zsh if it isn't already present
+  if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   fi
 }
 
@@ -94,14 +87,6 @@ declare -a FILES_TO_SYMLINK=(
   'bin'
   'powerline'
 )
-
-# Prompt to switch to zsh and oh-my-zsh if not active on terminal.
-if [ ! -f /bin/zsh -a ! -f /usr/bin/zsh -o ! -d $HOME/.oh-my-zsh/ ]; then
-  ask_for_confirmation "Switch to zsh and oh-my-zsh?"
-  if answer_is_yes; then
-    install_zsh
-  fi
-fi
 
 for i in ${FILES_TO_SYMLINK[@]}; do
   sourceFile="$(pwd)/$i"
@@ -127,3 +112,10 @@ sourceFile="$(pwd)/themes/pickles.zsh-theme"
 targetFile="$HOME/.oh-my-zsh/custom/pickles.zsh-theme"
 execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
 
+# Prompt to switch to zsh and oh-my-zsh if not active on terminal.
+if [ ! -f /bin/zsh -a ! -f /usr/bin/zsh -o ! -d $HOME/.oh-my-zsh/ ]; then
+  ask_for_confirmation "Switch to zsh and oh-my-zsh?"
+  if answer_is_yes; then
+    install_zsh
+  fi
+fi
