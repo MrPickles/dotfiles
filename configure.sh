2d282c2eee5311612171f7e031e34991d0e78a2b
Andrew Liu <liu.andrew.x@gmail.com>
2019-07-14 01:48:27 -0700

    Remove gitconfig.local and configure diff-so-fancy options in setup script

--- configure.sh
+++ configure.sh
@@ -178,8 +178,23 @@ if [[ $BUILD ]]; then
   # Link static gitignore.
   git config --global include.path ~/.gitconfig.static
 
+  # Set up diff-so-fancy.
   if [[ "$(command -v diff-so-fancy)" ]]; then
     git config --global pager.diff "diff-so-fancy | less --tabs=4 -RFX"
     git config --global pager.show "diff-so-fancy | less --tabs=4 -RFX"
+
+    git config --global color.ui true
+
+    git config --global color.diff-highlight.oldNormal    "red bold"
+    git config --global color.diff-highlight.oldHighlight "red bold 52"
+    git config --global color.diff-highlight.newNormal    "green bold"
+    git config --global color.diff-highlight.newHighlight "green bold 22"
+
+    git config --global color.diff.meta       "yellow"
+    git config --global color.diff.frag       "magenta bold"
+    git config --global color.diff.commit     "yellow bold"
+    git config --global color.diff.old        "red bold"
+    git config --global color.diff.new        "green bold"
+    git config --global color.diff.whitespace "red reverse"
   fi
 fi
