241566debe334b1bf9460666c6f47f2717b30e63
Andrew Liu <liu.andrew.x@gmail.com>
2019-07-14 01:27:34 -0700

    Configure diff-so-fancy options in non-static gitconfig
    The fix in 72f3cbb didn't quite work either because there was output as
    a side effect of running the type command. It really looks like there's
    no good solution, besides programmatically populating the results.

--- configure.sh
+++ configure.sh
@@ -177,4 +177,9 @@ if [[ $BUILD ]]; then
 
   # Link static gitignore.
   git config --global include.path ~/.gitconfig.static
+
+  if [[ "$(command -v diff-so-fancy)" ]]; then
+    git config --global pager.diff "diff-so-fancy | less --tabs=4 -RFX"
+    git config --global pager.show "diff-so-fancy | less --tabs=4 -RFX"
+  fi
 fi
