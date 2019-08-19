092fa497f660d18365b801b3d8dbddd527124e07
Andrew Liu <liu.andrew.x@gmail.com>
2019-08-19 00:07:32 -0700

    Add configs for nvim

--- configure.sh
+++ configure.sh
@@ -57,6 +57,10 @@ declare -a FILES_TO_SYMLINK=(
   'third_party/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh'
 )
 
+declare -a FULL_PATH_FILES_TO_SYMLINK=(
+  'config/nvim/init.vim'
+)
+
 print_success() {
   if [[ $BUILD ]]; then
     # Print output in green
@@ -140,29 +144,55 @@ install_zsh() {
   fi
 }
 
+link_file() {
+  local sourceFile=$1
+  local targetFile=$2
+
+  if [ ! -e "$targetFile" ]; then
+    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+  elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+    print_success "$targetFile → $sourceFile"
+  else
+    ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+    if answer_is_yes; then
+      rm -rf "$targetFile"
+      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    else
+      print_error "$targetFile → $sourceFile"
+    fi
+  fi
+}
+
+unlink_file() {
+  local sourceFile=$1
+  local targetFile=$2
+
+  if [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+    execute "unlink $targetFile" "$targetFile"
+  fi
+}
+
 # Symlink (or unlink) the dotfiles.
 for i in "${FILES_TO_SYMLINK[@]}"; do
   sourceFile="$(pwd)/$i"
   targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
   if [[ $BUILD ]]; then
-    if [ ! -e "$targetFile" ]; then
-      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-    elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-      print_success "$targetFile → $sourceFile"
-    else
-      ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-      if answer_is_yes; then
-        rm -rf "$targetFile"
-        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-      else
-        print_error "$targetFile → $sourceFile"
-      fi
-    fi
+    link_file $sourceFile $targetFile
   else
-    if [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-      execute "unlink $targetFile" "$targetFile"
-    fi
+    unlink_file $sourceFile $targetFile
+  fi
+done
+
+for i in "${FULL_PATH_FILES_TO_SYMLINK[@]}"; do
+  sourceFile="$(pwd)/$i"
+  targetFile="$HOME/.$i"
+
+  if [[ $BUILD ]]; then
+    mkdir -p $(dirname $targetFile)
+    link_file $sourceFile $targetFile
+  else
+    unlink_file $sourceFile $targetFile
   fi
 done
 
