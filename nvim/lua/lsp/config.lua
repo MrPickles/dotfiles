a1748911c4efb1d66f48767cd18e0bd3386318a0
Andrew Liu <liu.andrew.x@gmail.com>
2022-04-17 12:20:28 -0400

    Only install lua lsp by default

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -1,27 +1,9 @@
 local M = {}
 
--- All of the LSPs we'll install by default.
+-- Only install the Lua LSP by default.
+-- To install others take a look at :LspInstallInfo
 M.servers = {
-  'bashls', -- Bash
-  'clangd', -- C/C++
-  'clojure_lsp', -- Clojure
-  'dockerls', -- Docker
-  'gopls', -- Go
-  'graphql', -- GraphQL
-  'html', -- HTML
-  'jsonls', -- JSON
-  'jdtls', --Java
-  'ocamlls', --OCaml
-  'pyright', -- Python
-  'solargraph', -- Ruby
-  'sqlls', -- SQL
-  'sumneko_lua', -- Lua
-  'terraformls', -- Terraform
-  'tsserver', -- JavaScript/TypeScript
-  'vimls', -- VimL
-  'volar', -- Vue
-  'lemminx', -- XML
-  'yamlls', -- YAML
+  'sumneko_lua',
 }
 
 M.settings = {
