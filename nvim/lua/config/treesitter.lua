d988b0b097f57ae0a2277a119268becb93ac8661
Andrew Liu <liu.andrew.x@gmail.com>
2023-01-13 15:27:15 -0500

    Install tree-sitter parsers lazily

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,5 +1,10 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'all',
+  ensure_installed = {
+    "lua",
+    "vim",
+    "help",
+  },
+  auto_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
