e8449a02cdc13717d094e0a787156c721013eed8
Andrew Liu <liu.andrew.x@gmail.com>
2023-02-09 16:29:17 +0000

    Switch to tokyonight storm

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -7,7 +7,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme tokyonight-night")
+      vim.cmd("colorscheme tokyonight-storm")
     end,
   },
 
