2ee84e0bc4e60675ad6fd3268836e2c3fcc5c8c1
Andrew Liu <liu.andrew.x@gmail.com>
2023-05-12 11:31:33 -0400

    Create ubuntu apt script

---
+++ scripts/ubuntu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script installs most relevant pages for Ubuntu 22.04.
+
+sudo apt install \
+  bat \
+  build-essential \
+  curl \
+  exa \
+  fd-find \
+  fuse \
+  git \
+  libfuse2 \
+  ripgrep \
+  sudo \
+  tmux \
+  tree \
+  unzip \
+  vim \
+  wget \
+  zsh
