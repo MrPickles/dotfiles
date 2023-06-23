85d54f9929bb0128c574aad6e5da7b07e5ec0098
Andrew Liu <liu.andrew.x@gmail.com>
2023-06-23 11:05:21 -0400

    Fetch latest theme release rather than hardcode

--- setup.sh
+++ setup.sh
@@ -95,7 +95,7 @@ install_omz() {
   # Clone or update Powerlevel10k.
   THEME_REPO_URL="https://github.com/romkatv/powerlevel10k"
   THEME_PATH="${ZSH_CUSTOM}/themes/${THEME_REPO_URL##*/}"
-  THEME_VERSION_TAG="v1.19.0"
+  THEME_VERSION_TAG=$(curl -s https://api.github.com/repos/romkatv/powerlevel10k/releases/latest | jq -r .tag_name)
   if [[ ! -d "${THEME_PATH}" ]]; then
     git clone --quiet --filter=blob:none --branch "${THEME_VERSION_TAG}" "${THEME_REPO_URL}" "${THEME_PATH}"
   else
