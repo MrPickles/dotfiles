e14a5c4b0604446b6d083a93f985d6b9518347b3
Andrew Liu <liu.andrew.x@gmail.com>
2023-10-09 16:52:09 -0400

    Update ubuntu setup script

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 # This script installs most relevant packages for Ubuntu 22.04.
-#
-# TODO: Eventually, it might be good to add a few extras:
-# - Installing git-delta
-# - Installing neovim
-# - Properly linking fd-find
-# Most of these can be copied from the Dockerfile.
+_has() {
+  return $(whence $1 >/dev/null)
+}
 
 sudo apt install \
   bat \
@@ -24,9 +21,31 @@ sudo apt install \
   wget \
   zsh
 
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
+if ! _has nvim; then
+  wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+  chmod +x nvim.appimage
+  ./nvim.appimage --appimage-extract
+  sudo mv squashfs-root /
+  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+  rm nvim.appimage
+fi
+
+if ! _has delta; then
+  wget https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb
+  sudo dpkg -i git-delta-musl_0.15.1_amd64.deb
+  rm git-delta-musl_0.15.1_amd64.deb
+fi
+
+if ! _has eza; then
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
+fi
+
+if ! _has fd; then
+  mkdir -p ~/.local/bin
+  ln -s $(which fdfind) ~/.local/bin/fd
+fi
