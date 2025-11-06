e3102625587e23ea5150f467d16e94c02e1a96f5
Andrew Liu <liu.andrew.x@gmail.com>
2025-11-06 01:02:30 -0500

    Switch to using native nvim lsp config
    Setting it up via plugin is deprecated.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -83,7 +83,7 @@ return {
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
-        require("lspconfig")[server].setup({
+        vim.lsp.enable(server, {
           on_attach = on_attach,
           capabilities = capabilities,
           settings = {
